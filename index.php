032742370c8b5ea28e566a78350198a3c8dce61e
Nate Smith <me@nwlsmith.com>
2019-02-12 14:05:39 -0500

    fixed image

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<h1>Hello World!</h1>
-<img src='/images/cat.jpeg' alt='Adorable kitten'>
+<!doctype html>
+<html>
+    <head>
+        <title>Index</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <img src="images/cat.jpg" alt="Adorable kitten">
+    </body>
+</html>
